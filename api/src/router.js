70fb884351e717ec83a2cb0823f799bc31f40ecd
Kay <joe_kayu@yahoo.com>
2020-08-16 09:54:04 +0100

    .

---
+++ api/src/router.js
@@ -0,0 +1,30 @@
+const router = require('express').Router;
+const multer = require('multer');
+
+const filename = (request, file, callback) => {
+    callback(null, file.originalname)
+}
+const storage = multer.diskStorage({destination: 'api/uploads', filename});
+
+const fileFilter = (request, file, callback) => {
+    if (request.fileValidatorError === 'Wrong file type'){
+        callback(null, false, new Error('Wrong file type'))
+    } else {
+        callback(null, true);
+    }
+}
+
+const upload = multer({
+    fileFilter, storage
+})
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+    if (req.fileValidatorError){
+        res.status(400).json({error: req.fileValidatorError})
+    }
+    if (!req.fileValidatorError){
+        res.status(201).json({success: true})
+    }
+})
+
+module.exports = router;
