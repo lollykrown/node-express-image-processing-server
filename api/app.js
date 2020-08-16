5414e2e090593673b30fb11dbba92cbf9c5b0b3c
Kay <joe_kayu@yahoo.com>
2020-08-16 10:20:09 +0100

    .

--- api/app.js
+++ api/app.js
@@ -7,9 +7,11 @@ const app = express();
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
 app.use('/', router);
-app.use(express.static(path.resolve(__dirname, 'uploads')))
-app.use('/*', (req, res) => {
-    res.sendFile(pathToIndex)
-})
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
+app.use('/*', (request, response) => {
+  response.sendFile(pathToIndex);
+});
 
 module.exports = app;
+
